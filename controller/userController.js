a89adc94434f1f03f322d476d3817b52ac0714d4
abreham <abrehamnigus1996@gmail.com>
2025-09-14 20:01:28 +0300

    register

--- controller/userController.js
+++ controller/userController.js
@@ -1,39 +1,38 @@
 //db connection
 const db = require('../db/dbconfige');
 const bcrypt = require('bcryptjs');
+const {StatusCode} = require('http-status-codes');
 
 async function register(req, res) {
    const { username, password, firstname, lastname, email } = req.body;
    if (!username || !password || !firstname || !lastname || !email) {
-         return res.status(400).json({ message: 'All fields are required' });
+         return res.status(StatusCode.BAD_REQUEST).json({ message: 'All fields are required' });
    }
    try {
     const [user] = await db.query('SELECT username,userid FROM users WHERE username = ? or email = ?', [username, email]);
     if (user.length > 0) {
-        return res.status(400).json({ message: 'Username or email already exists' });
+        return res.status(StatusCode.BAD_REQUEST).json({ message: 'Username or email already exists' });
     }
    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
-    return res.status(400).json({
+    return res.status(StatusCode.BAD_REQUEST).json({
         message: 'Password must be at least 8 characters long, contain uppercase, lowercase, number, and special character'
     });
 }
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    return res.status(400).json({
+    return res.status(StatusCode.BAD_REQUEST).json({
         message: 'Invalid email format'
     });
 }
     const hashedPassword = await bcrypt.hash(password, 10);
+
     // Store user in the database
-
-
-
     await db.query('INSERT INTO users (username, password, firstname, lastname, email) VALUES (?, ?, ?, ?, ?)',
         [username, hashedPassword, firstname, lastname, email]);
-    return res.status(201).json({ message: 'User registered successfully' });
+    return res.status(StatusCode.CREATED).json({ message: 'User registered successfully' });
    }
     catch (error) {
         console.log(error.message);
-        return res.status(500).json({ message: 'Server error' });
+        return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
     }
 }
 async function login(req, res) {
