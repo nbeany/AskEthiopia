09fbac77c88f7364e01160c096f54730c1fad385
abreham <abrehamnigus1996@gmail.com>
2025-09-16 10:29:44 +0300

    login page

--- controller/userController.js
+++ controller/userController.js
@@ -1,25 +1,25 @@
 //db connection
 const db = require('../db/dbconfige');
 const bcrypt = require('bcryptjs');
-const {StatusCode} = require('http-status-codes');
+const {StatusCodes} = require('http-status-codes');
 
 async function register(req, res) {
    const { username, password, firstname, lastname, email } = req.body;
    if (!username || !password || !firstname || !lastname || !email) {
-         return res.status(StatusCode.BAD_REQUEST).json({ message: 'All fields are required' });
+         return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields are required' });
    }
    try {
     const [user] = await db.query('SELECT username,userid FROM users WHERE username = ? or email = ?', [username, email]);
     if (user.length > 0) {
-        return res.status(StatusCode.BAD_REQUEST).json({ message: 'Username or email already exists' });
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Username or email already exists' });
     }
    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
-    return res.status(StatusCode.BAD_REQUEST).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
         message: 'Password must be at least 8 characters long, contain uppercase, lowercase, number, and special character'
     });
 }
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    return res.status(StatusCode.BAD_REQUEST).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
         message: 'Invalid email format'
     });
 }
@@ -28,27 +28,28 @@ async function register(req, res) {
     // Store user in the database
     await db.query('INSERT INTO users (username, password, firstname, lastname, email) VALUES (?, ?, ?, ?, ?)',
         [username, hashedPassword, firstname, lastname, email]);
-    return res.status(StatusCode.CREATED).json({ message: 'User registered successfully' });
+    return res.status(StatusCodes.CREATED).json({ message: 'User registered successfully' });
    }
     catch (error) {
         console.log(error.message);
-        return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
     }
 }
 async function login(req, res) {
    const { email, password } = req.body;
    if (!email || !password) {
-       return res.status(StatusCode.BAD_REQUEST).json({ message: 'Email and password are required' });
+       return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email and password are required' });
    }
    try {
        const [user] = await db.query('SELECT username,userid, password FROM users WHERE email = ?', [email]);
-       if (user.length == 0) {
-           return res.status(StatusCode.BAD_REQUEST).json({ message: 'Invalid email or password' });
-       }
+       return res.status(StatusCodes.OK).json({ user: user });
+    //    if (user.length == 0) {
+    //        return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid email or password' });
+    //    }
       
    } catch (error) {
        console.log(error.message);
-       return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
+       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
    }
 }
 
